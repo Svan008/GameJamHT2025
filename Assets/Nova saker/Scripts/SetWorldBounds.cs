1c488bfa452cca993e5c76a23d77ba0be07cd291
Novi-ish <nova.vidmark@gmail.com>
2025-11-06 11:27:26 +0100

    camera bonderies

---
+++ Assets/Nova saker/Scripts/SetWorldBounds.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetWorldBounds : MonoBehaviour
+{
+    private void Awake()
+    {
+        var bounds = GetComponent<BoxCollider2D>().bounds;
+        Globals.WorldBounds = bounds;
+    }
+
+}
