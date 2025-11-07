e19eeedf0a554c39ea79a6324f5065b82860aed1
OliwerWesther <Oliwer.westher@elev.ga.lbs.se>
2025-11-07 13:22:40 +0100

    .

--- Assets/Nova saker/Scripts/BossPath.cs
+++ Assets/Nova saker/Scripts/BossPath.cs
@@ -85,6 +85,7 @@ public class BossPath : MonoBehaviour
         Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
         Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
         Gizmos.DrawWireSphere(PointC.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(PointD.transform.position, 0.5f);
         Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
         // gör punkterna tydligare
     }
