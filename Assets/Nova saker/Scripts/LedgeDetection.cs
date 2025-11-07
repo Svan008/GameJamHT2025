0b7ce8d431ebdb1e40d42cb1c9a1e18b0cbb2f9a
Novi-ish <nova.vidmark@gmail.com>
2025-11-07 11:55:02 +0100

    wall climb(inte färdig)

---
+++ Assets/Nova saker/Scripts/LedgeDetection.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgeDetection : MonoBehaviour
+{
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private movement player;// referar till movement scriptet
+
+    private bool canDetect;
+
+    private void Update()
+    {
+        if(canDetect)
+        player.ledgeDetected = Physics2D.OverlapCircle(transform.position, radius, whatIsGround);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("ground"))
+            canDetect = false;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("ground"))
+            canDetect = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+}
